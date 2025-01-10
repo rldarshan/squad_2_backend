596b7dac808691c9ddc8d9ffba3c86077e8f28cc
rldarshan <rl.darshan01@gmail.com>
2025-01-10 15:15:06 +0530

    Added get_all_patients API

--- models/healthTipsModel.js
+++ models/healthTipsModel.js
@@ -4,21 +4,7 @@ const healthTipsSchema = new mongoose.Schema({
     age_group: {
       type: String,
       required: true,
-    },
-    data: [
-      {
-        title: {
-          type: String,
-          required: true,
-        },
-        tips: [
-          {
-            type: String,
-            required: true,
-          },
-        ],
-      },
-    ],
+    }
   });
 
 module.exports = mongoose.model('HealthTips', healthTipsSchema);
